6fbccca1c5c702525ec2e71b5ddbc9018c4277df
mooilicht <62236303+mooilicht@users.noreply.github.com>
2022-04-25 11:26:14 +0900

    blog #1 블로그 만들기

---
+++ _nuxt/static/1650853355/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/", (function(a,b){return {data:[{list:[{slug:"hello",title:"Getting started",category:a},{slug:"hello2",title:"Hello2",category:a},{slug:"hello3",title:b,category:b}]}],fetch:{},mutations:void 0}}("Hello","Hello3")));
